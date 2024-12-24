22ff399b039233628723c228fe44245726cc700a
kiki769 <fiirve@bk.ru>
2024-12-24 23:21:37 +0500

    Sprint7

--- Tyuiu.FilatovDK.Sprint7.Project.V13/FormMain.cs
+++ Tyuiu.FilatovDK.Sprint7.Project.V13/FormMain.cs
@@ -175,7 +175,7 @@ namespace Tyuiu.FilatovDK.Sprint7.Project.V13
 
         private void buttonRed_FDK_Click(object sender, EventArgs e)
         {
-            dataGridViewInfo_FDK.ReadOnly = false;
+            dataGridViewInfo_FDK.ReadOnly = false;//Позволяет редактировать таблицу
         }
     }
 }
